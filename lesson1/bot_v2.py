011512ff69bef6278ff6ae10ee9ac01ab506cda2
Alexandr Karpukhin <alkarpukhin@gmail.com>
2017-05-31 23:16:18 +0300

    fixes

--- lesson1/bot_v2.py
+++ lesson1/bot_v2.py
@@ -1,5 +1,7 @@
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -8,16 +10,22 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 
 
 def start_bot(bot, update):
-    print(update)
     mytext = "Привет {}! Я простой бот и понимаю только команду {}".format(update.message.chat.first_name, '/start')
 
     update.message.reply_text(mytext)
 
 
+def chat(bot, update):
+    text = update.message.text
+    logging.info(text)
+    update.message.reply_text(text)
+
+
 def main():
-    updtr = Updater("366340595:AAFyTYshBiFi9_ornCx2LnyDfl6sgI0Cmmg")
+    updtr = Updater(settings.TELEGRAM_API_KEY)
 
     updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
     updtr.start_polling()
     updtr.idle()
